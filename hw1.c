6cf5fe86a834b6377c8f16abf276fbb09d7bc6a9
CJ from Alienware <hongcheng1wu@gmail.com>
2020-12-14 20:36:26 -0600

    complete

---
+++ hw1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "hw1.h"
+
+int main(){
+
+	I_have_written();
+	that_you_needed();
+	the_code[0] = 1;
+	and_which();
+	int val = printf("xwu82");
+	
+
+	return val;
+}
+void Forgive_me(){
+	return;
+}
+int they_are_arbitrary;
+int so_random[2] = {1,2};
+void and_so_varied(){
+        return;
+}
---
+++ hw1.h
@@ -0,0 +1,20 @@
+static int I_have_written(){
+	return 0;
+}
+static int the_code[3] = {1,2,3};
+//static char the_code2[12] = {'a','a','a','a','a','a','a','a','a','a','a','a'};
+static int that_you_needed(){
+	static int to_compile[1] = {1};
+	return to_compile[1];
+}
+static char and_which(){
+	static int symbols[2];
+	static int ridiculous[1] = {1};	
+	static int has_a_bunch_of[1] = {1};
+	symbols[0] = has_a_bunch_of[1] + ridiculous[0];
+	return symbols[2];
+}
+int sides_and(){
+	return 0;
+}
+
